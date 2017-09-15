215abd458be1d5018eb17ae61bfcab0678a409a7
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-09-15 12:30:06 -0600

    Added instantiation in controller of the model and fixed errors.

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -16,18 +16,18 @@ public class MarshmallowMonster
 		
 	}
 	
-	public MarshmallowMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)
+	public MarshmallowMonster(String name, int eyeCount, int arms, double tentacleAmount, boolean hasBloop)
 	{
 		this.name = name;
 		this.eyeCount = eyeCount;
 		this.armCount =arms;
-		this.tentacleAmount = tentacleCount;
+		this.tentacleAmount = tentacleAmount;
 		this.hasBloop = hasBloop;
 	}
 	
 	public String toString()
 	{
-		String description = "I am a monster, my name is " + name + ", I have " + eyeCount + " eyes and only " + tentacleAmount + "tentacles but I have " + armCount + " arms!" + hasBloop + " tells you if I have a bloop";
+		String description = "I am a monster, my name is " + name + ", I have " + eyeCount + " eyes and only " + tentacleAmount + " tentacles but I have " + armCount + " arms!" + hasBloop + " tells you if I have a bloop";
 		return description;
 		
 	}
