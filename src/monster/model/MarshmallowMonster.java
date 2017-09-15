765f92b4a42e1b5560c8821707346b7814143af6
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-09-15 12:01:53 -0600

    Made MarshmallowMonster model and added data members.

---
+++ src/monster/model/MarshmallowMonster.java
@@ -0,0 +1,27 @@
+package monster.model;
+
+public class MarshmallowMonster
+{
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	public MarshmallowMonster()
+	{
+		//initializes everything to 0, false or null.
+		
+	}
+	
+	public MarshmallowMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)
+	{
+		this.name = name;
+		this.eyeCount = eyeCount;
+		this.armCount =arms;
+		this.tentacleAmount = tentacleCount;
+		this.hasBloop = hasBloop;
+	}
+}
