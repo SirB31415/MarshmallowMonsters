7d942d50ae792156df08aa94117419db8eafb8ad
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-09-19 13:55:14 -0600

    Added a popup

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,10 +1,20 @@
 package monster.controller;
 
+import monster.view.MonsterDisplay;
 import monster.model.MarshmallowMonster;
 import java.util.Scanner;
 
+
+
 public class MonsterController
 {
+	private MonsterDisplay popup;
+	
+	public MonsterContoller()
+	{
+		popup = new MonsterDisplay();
+	}
+	
 	public void start()
 	{
 		MarshmallowMonster basic = new MarshmallowMonster();
@@ -26,9 +36,17 @@ public class MonsterController
 		System.out.println(currentMonster);
 		
 		System.out.println(currentMonster.getName() + " wants to know how many tentacles you finna eat");
-		int consumedTentacles = myScanner.nextInt();
-		currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - consumedTentacles);
-		System.out.println(currentMonster);
+		double consumedTentacles = myScanner.nextDouble();
+		if(consumedTentacles == currentMonster.getTentacleAmount())
+		{
+			System.out.println("You ate all my tentacles");
+		}
+		else
+		{
+			currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - consumedTentacles);
+			System.out.println(currentMonster);
+		}
+		
 		
 		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
 		//consumed = myScanner.nextInt();
@@ -41,11 +59,19 @@ public class MonsterController
 		{
 			System.out.println("Math is hard for you, this is impossible.");
 		}
+		else if (armEat > currentMonster.getArmCount())
+		{
+			System.out.println("You can't eat more arms than I have");
+		}
 		else
 		{
 			currentMonster.setArmCount(currentMonster.getArmCount() - armEat);
 			System.out.println("Okay, now I have " + currentMonster.getArmCount());
 		}
+		
+		popup.displayText("Hi there ready to play???");
+		String answer = popup.getResponse("What is the air speed of a coconut laden swallow?");
+		System.out.println(answer);
 		
 		myScanner.close();
 	}
